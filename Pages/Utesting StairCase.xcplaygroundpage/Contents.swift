1ea5356e5056c49d32e72de11f7e5a50a8ebb6e1
JuanHarrington <Harringtonjuanc@gmail.com>
2022-07-15 20:45:40 -0500

    algorithm renaming tests and functions, and adding some explanation about the exercise

--- Pages/Utesting StairCase.xcplaygroundpage/Contents.swift
+++ Pages/Utesting StairCase.xcplaygroundpage/Contents.swift
@@ -2,9 +2,23 @@ import Foundation
 import XCTest
 import Foundation
 
+///* This algorithim if for count how many time is the biggest value (a candle in this case) is repeated */
 
-struct Algorithms{
-    func birthdayCakeCandles(candles: [Int]) -> Int {
+/*
+ Given five positive integers, find the minimum and maximum values that
+ can be calculated by summing exactly four of the five integers.
+ Then print the respective minimum and maximum values as a single
+ line of two space-separated long integers.
+ 
+ EXAMPLE:
+ arr: [1,3,5,7,9]
+ 
+ The minimum sum is 1 + 3 + 5 + 7 = 16  and the maximum sum is 3 + 5 + 7 + 9 = 24 .
+ Then the funciton should return [16,24]
+ */
+
+struct Algorithm{
+    func biggerBirthdayCakeCandlesCounter(candles: [Int]) -> Int {
         var counter = 0
         let maxNumber = candles.max()
         
@@ -19,7 +33,7 @@ struct Algorithms{
 }
 
 class AlgorithmsTests: XCTestCase {
-    var algorithms: Algorithms!
+    var algorithms: Algorithm!
     
     override func setUp() {
         super.setUp()
@@ -27,37 +41,48 @@ class AlgorithmsTests: XCTestCase {
     }
     
     
-    func test1() {
+    func testingMinimumBorderCase() {
         // Given
         let givenGrade = [1,3,3,2]
         
         //When
-        let result = algorithms.birthdayCakeCandles(candles: givenGrade)
+        let result = algorithms.biggerBirthdayCakeCandlesCounter(candles: givenGrade)
         
         // Then
         XCTAssertEqual(result, 2)
     }
     
-    func test2() {
+    func testingMaximumBorderCase() {
         // Given
         let givenGrade = [44,84,94,21,0,18,100,18,62,100,61,53,0,43,2,100,53,61,40,14]
 
         //When
-        let result = algorithms.birthdayCakeCandles(candles: givenGrade)
+        let result = algorithms.biggerBirthdayCakeCandlesCounter(candles: givenGrade)
         
         // Then
         XCTAssertEqual(result, 3)
     }
     
-    func test3() {
+    func testingTooMuchRepetitionsOfOneSingleValue() {
         // Given
-        let givenGrade = [1,2,3,4,4,4]
+        let givenGrade = [1,2,3,4,4,4,4,4,4]
 
         //When
-        let result = algorithms.birthdayCakeCandles(candles: givenGrade)
+        let result = algorithms.biggerBirthdayCakeCandlesCounter(candles: givenGrade)
         
         // Then
-        XCTAssertEqual(result, 3)
+        XCTAssertEqual(result, 6)
+    }
+    
+    func testingJustOneRepetitionOfOneSingleValue() {
+        // Given
+        let givenGrade = [1,2,3,4,5,6,7,8,9,10,10]
+
+        //When
+        let result = algorithms.biggerBirthdayCakeCandlesCounter(candles: givenGrade)
+        
+        // Then
+        XCTAssertEqual(result, 2)
     }
 }
 
